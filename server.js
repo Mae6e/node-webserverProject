4c5da1f53548fcc36b74ff1268a1c02c26c2883f
Maede <maedehzebhi@gmail.com>
2021-02-13 22:01:52 +0330

    First commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const hbs=require('hbs');
+var app = express();
+var fs = require('fs');
+
+app.use(express.static(`${__dirname}/public`));
+app.set('view engine','hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+hbs.registerHelper('upperCase',(text)=>{
+     return text.toUpperCase();
+});
+hbs.registerHelper('currentDate',()=>{
+    return new Date().getFullYear();
+})
+
+app.use((req,res,next)=>{
+    var log = `${new Date()} ${req.method} ${req.url}`;
+    fs.writeFileSync('server.log',log);
+    next();
+})
+
+app.use((req,res,next)=>{
+     res.render('offline.hbs');
+})
+
+app.get('/',(req,res)=>{
+   res.send({
+       pageTitle:'Home',
+       currentDate:new Date().getFullYear()
+   })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('server run on port 3000');
+});
